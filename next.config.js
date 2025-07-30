8a6b9113f3b121cd4524ae44f7af3f359bdfa404
sumitchaudhary980 <jaiswalsumit1010@gmail.com>
2025-07-30 19:24:30 +0545

    change is nextconfigjs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: false,
   images: {
     domains: ["avatars.githubusercontent.com"],
   },
-  output: "standalone",
+  output: "export", // <- THIS is key for static sites
 };
+
+module.exports = nextConfig;
