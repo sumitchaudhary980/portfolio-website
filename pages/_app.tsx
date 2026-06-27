b58b8865194cfb196da56cc6163fa06c9f18053c
Sumit Kumar Chaudhary <jaiswalsumit1010@gmail.com>
2026-06-27 23:08:01 +0545

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import { AppProps } from "next/app";
+import Head from "next/head";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/argon-design-system-react.css";
 import "../styles/styles.css";
@@ -6,7 +8,18 @@ import "../styles/vendor/font-awesome/css/font-awesome.min.css";
 import "../styles/vendor/nucleo/css/nucleo.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="XJBBUDtDebx06rVkc1jkqmhCN0uPEuELkt9XnZmIlKY"
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
